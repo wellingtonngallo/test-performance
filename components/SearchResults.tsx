d304bc1186f04c10ff21c03112c210afb488409b
Wellington Gallo <wellinton.gallo77@gmail.com>
2021-09-18 14:14:00 -0300

    feat: test useMemo

--- components/SearchResults.tsx
+++ components/SearchResults.tsx
@@ -1,3 +1,4 @@
+import { useMemo } from 'react';
 import { ProductItem } from './ProductItem';
 
 type SearchResultsProps = {
@@ -9,8 +10,15 @@ type SearchResultsProps = {
 }
 
 export function SearchResults({ results }: SearchResultsProps) {
+  const totalPrice = useMemo(() => {
+    return results.reduce((total, product) => {
+      return total + product.price;
+    }, 0);
+  }, [results]);
+  
   return (
     <div>
+      <h2>{totalPrice}</h2>
       {results.map(product => {
         return (
           <ProductItem key={product.id} product={product} />
